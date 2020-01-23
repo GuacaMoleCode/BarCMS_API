a7b2be456d45152d752d63d79459c14d81cb7c08
GuacaMollee <dmollee@dearnova.nl>
2020-01-23 19:10:18 +0100

    Added models for website

--- api/headersliderpages/models/Headersliderpages.js
+++ api/headersliderpages/models/Headersliderpages.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Lifecycle callbacks for the `Slider` model.
+ * Lifecycle callbacks for the `Headersliderpages` model.
  */
 
 module.exports = {
--- api/siteconfiguration/models/Siteconfiguration.js
+++ api/siteconfiguration/models/Siteconfiguration.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Lifecycle callbacks for the `Sliderpagina` model.
+ * Lifecycle callbacks for the `Siteconfiguration` model.
  */
 
 module.exports = {
